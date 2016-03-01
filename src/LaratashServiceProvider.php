75a7ef95b7f483e4564a986715df10e302e57951
Juuso Piikkilä <juuso@meiko.fi>
2016-03-01 15:09:46 +0200

    add blade compiler

--- src/LaratashServiceProvider.php
+++ src/LaratashServiceProvider.php
@@ -27,6 +27,25 @@ class LaratashServiceProvider extends ServiceProvider
     }
 
     /**
+     * Register the Blade engine implementation.
+     *
+     * @param  \Illuminate\View\Engines\EngineResolver  $resolver
+     * @return void
+     */
+    public function registerBladeEngine($resolver)
+    {
+        $app = $this->app;
+        // The Compiler engine requires an instance of the CompilerInterface, which in
+        // this case will be the Blade compiler, so we'll first create the compiler
+        // instance to pass into the engine so it can compile the views properly.
+        $app->singleton('blade.compiler', function($app)
+        {
+            $cache = $app['config']['view.compiled'];
+            return new BladeCompiler($app['files'], $cache);
+        });
+    }
+
+    /**
      * Get the services provided by the provider.
      *
      * @return array
@@ -46,6 +65,11 @@ class LaratashServiceProvider extends ServiceProvider
     {
         $this->app->bind('mustache.engine', function() {
             return $this->app->make('Laratash\MustacheEngine');
+        });
+
+        $this->app->extend('view.engine.resolver', function($resolver, $app) {
+            $this->registerBladeEngine($resolver);
+            return $resolver;
         });
     }
 
